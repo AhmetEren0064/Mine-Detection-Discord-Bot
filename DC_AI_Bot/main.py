207b41e6892b12b43cb1ccb9c27ceed91863a6e3
AhmetEren0064 <162623099+AhmetEren0064@users.noreply.github.com>
2025-03-07 16:01:55 +0300

    Maden tanıyan bir bot.

---
+++ DC_AI_Bot/main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from model import check_picture
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachments in ctx.message.attachments:
+            file_name = attachments.filename
+            await attachments.save(f"./img/{attachments.filename}")
+            await ctx.send(check_picture(resim=f"./img/{attachments.filename}"))
+    else:
+        await ctx.send("Bir fotoğraf eklemelisiniz")
+    
+
+bot.run("MTIzMTMzMDYwMDM2Nzk0Nzg0Nw.G1qVU4.0dk1miZMtvbxGhQFFlUWX5qX-5bhAaG67VR0lU")
